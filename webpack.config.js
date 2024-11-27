f362b937707cb9e28da80b88af71eebb52f4ef93
Havenobrain <107768179+Havenobrain@users.noreply.github.com>
2024-11-27 13:24:51 +0400

    Add fallback for callback routing

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', // Точка входа
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+        filename: 'bundle.js', // Имя выходного файла JavaScript
+        path: path.resolve(__dirname, 'dist'), // Папка для сборки
+        clean: true, // Очистка папки dist перед сборкой
+        publicPath: '/pipedrive_app/', // Базовый путь для деплоя на GitHub Pages
     },
-    mode: 'development',
+    mode: 'development', // Режим разработки
     devServer: {
-        static: './dist',
-        open: true,
-        hot: true,
+        static: './dist', // Папка для сервера разработки
+        open: true, // Открытие браузера после старта сервера
+        hot: true, // Горячая перезагрузка
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.css$/, // Обработка CSS-файлов
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/, // Обработка изображений
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/, // Обработка шрифтов
+                type: 'asset/resource',
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
+            template: './src/index.html', // Шаблон для главной страницы
+            filename: 'index.html', // Имя выходного файла
         }),
         new HtmlWebpackPlugin({
-            template: './src/callback.html',
-            filename: 'callback.html',
+            template: './src/callback.html', // Шаблон для callback страницы
+            filename: 'callback.html', // Имя выходного файла
         }),
     ],
 };
