33cb7fe89d086fa4261cb8e9f847439047d1e38a
Havenobrain <107768179+Havenobrain@users.noreply.github.com>
2024-11-27 11:31:14 +0400

    Add callback.html to build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js', 
@@ -33,7 +34,12 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html', 
+            template: './src/index.html', // Главная страница
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/callback.html', to: 'callback.html' }, // Копируем callback.html
+            ],
         }),
     ],
 };
